92c32b7daefd9e74cd7d1151d208663bfad3ee24
ALAN DALY <alan.daly@ie.ibm.com>
2021-02-15 12:05:28 +0000

    Changed display text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Hello how are things ?
+          I was changed on the feature branch ?
           </p>
          <p> 
            </p> 
